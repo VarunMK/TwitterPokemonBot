eab97c62825dfb4b7d03d6c400d5fef4e8b1df5d
Varun Mk <mkvarun2001@gmail.com>
2021-04-29 13:56:54 +0530

    Final Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Twitter PokeBot
+* A simple twitter bot made using the tweepy library.
+* It tweets a random pokemon image whenever someone tweets and tags me.
+* I was bored and I wanted to checkout the twitter API so I decided to make this.
+### Resources used:
+* [Tweepy Documentation](https://docs.tweepy.org/en/latest/index.html)
+* [Dataset](https://www.kaggle.com/kvpratama/pokemon-images-dataset)
---
+++ pokebot.py
@@ -0,0 +1,46 @@
+import tweepy
+import time
+import os
+import random
+
+token= os.getenv('TOKEN')
+token_secret= os.getenv('TOKEN_SECRET')
+consumer_key= os.getenv('CONSUMER_KEY')
+consumer_secret=os.getenv('CONSUMER_SECRET')
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(token, token_secret)
+
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+def readlastseen(fname):
+    fread=open(fname,'r')
+    lastseen=int(fread.read().strip())
+    fread.close()
+    return lastseen
+
+def storelastseen(fname,content):
+    fwrite=open(fname,'w')
+    fwrite.write(str(content))
+    fwrite.close()
+
+def reply():
+    lastseen=readlastseen('lastseen.txt')
+    mentions=api.mentions_timeline(lastseen,tweet_mode='extended')
+    for m in reversed(mentions):
+        if not m:
+            return
+        print(str(m.id) + ' - ' + m.full_text)
+        lastseen=m.id
+        storelastseen('lastseen.txt',lastseen)
+        text=m.full_text.lower()
+        if('adopt a pokemon' in text or 'adopt a pokémon' in text):
+            api.update_with_media(filename='./pokemon_jpg/'+str(random.randint(1,794))+'.jpg',status='@'+m.user.screen_name+' Hey, here is your pokemon pet!',in_reply_to_status_id=m.id)
+        elif('are you ready' in text and m.user.screen_name=='zotako01'):
+            api.update_with_media(filename='aye.png',status='@'+m.user.screen_name+' o7',in_reply_to_status_id=m.id)
+while True:
+    try:
+        reply()
+    except Exception:
+        pass
+    time.sleep(15)
